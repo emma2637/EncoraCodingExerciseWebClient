8965a3fad91c32f29b5fdb09d802e8a157e874d8
Emmanuel Carvajal <emma2637@hotmail.com>
2021-02-08 20:05:29 -0600

    Addding Add Button And changing edit behavior

--- src/app/property-details/property-details.component.ts
+++ src/app/property-details/property-details.component.ts
@@ -10,15 +10,19 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./property-details.component.css']
 })
 export class PropertyDetailsComponent implements OnInit {
-
+  //  isEditActive:boolean = false;
   //constructor(public service:PropertyDetailService, public formComponent:PropertyDetailFromComponent , private toastr:ToastrService ) { }
-  constructor(public service:PropertyDetailService
-    ) { }
+  constructor(public service:PropertyDetailService ) { }
   ngOnInit(): void {
     this.service.refreshList();//initialize the list
   }
   populateForm(seletedRow:PropertyDetail){
     this.service.formData = Object.assign({},seletedRow) ;//this object assign is used for avoiding to change the row at the same time the form does
   }
- 
+  createForm(){
+    this.service.formData = new PropertyDetail();
+  //  this.isEditActive = false;
+  }
+
+  
 }
